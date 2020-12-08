96354f665c079e47b66c267492422497102f8c69
Alexandr Hovsepyan <alexandrhovsepyan@yandex.ru>
2020-12-08 19:05:12 +0400

    add redis image db-compose

--- postgres/init_db.sql
+++ postgres/init_db.sql
@@ -1,5 +1,3 @@
 SELECT 'CREATE DATABASE challenge'
 WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'challenge')\gexec
 GRANT ALL PRIVILEGES on challenge.* to root
---TO 'root'@'%' IDENTIFIED BY 'root'
---WITH GRANT OPTION;
